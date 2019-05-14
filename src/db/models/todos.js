c804e663fc97cc82ce27da3ae4148fcb7db2599c
Mosinmiloluwa Owoso <mosinmiloluwaowoso@Mosinmiloluwas-MacBook-Pro.local>
2019-05-14 14:23:36 +0100

    refactore sequelize setup

---
+++ src/db/models/todos.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const Todos = sequelize.define('Todos', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {});
+  Todos.associate = (models) => {
+    Todos.belongsTo(models.User, {
+      foreignKey: 'id',
+      as: 'user_id',
+    });
+  };
+  return Todos;
+};
